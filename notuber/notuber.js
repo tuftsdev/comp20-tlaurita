cfae73b90257ad018209d8d6f74c4507667c2bec
Teddy Laurita <teddy.laurita@gmail.com>
2017-02-19 23:42:17 -0500

    map initializes to user's current location or Boston if no geolocation

--- notuber/notuber.js
+++ notuber/notuber.js
@@ -2,12 +2,44 @@
 // Author: Teddy Laurita
 
 notuber = {
+    "latitude": 42.364506,
+    "longitude": -71.038887,
     "initMap": function initMap() {
-        this.map = new google.maps.Map(document.getElementById("map"), {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 8
-          });
+        if (navigator.geolocation) {
+            // Check if browser allows for geolocation
+            navigator.geolocation.getCurrentPosition(function(position) {
+                initialLocation = new google.maps.LatLng(
+                    this.latitude = position.coords.latitude,
+                    this.longitude = position.coords.longitude
+                )});
+            this.map = new google.maps.Map(document.getElementById("map"), {
+                center: {lat: this.latitude, lng: this.longitude},
+                zoom: 8
+            });
+        }
+        else {
+            // if no geolocation, open map on Boston, MA
+            this.map = new google.maps.Map(document.getElementById("map"), {
+                center: {lat: this.latitude, lng: this.longitude},
+                zoom: 8
+              });
+        }
+
     },
-    "latitude": 0.0,
-    "longitude": 0.0
+    "getLocation": function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                this.latitude = position.coords.latitude;
+                this.longitude = position.coords.longitude;
+            });
+        }
+        else {
+            document.getElementById("infoPanel").innerHTML =
+                                "Geolocation not supported by this browser";
+        }
+    },
+}
+
+window.onload = function() {
+    notuber.getLocation();
 }
